4c41aa1d85bda6783f3205abd355302c10b744cf
Blessing Hirwa <hirwablessing7@gmail.com>
2024-02-16 08:45:11 +0200

    add display menu options

---
+++ main.sh
@@ -0,0 +1,10 @@
+FILE="students-list_1023.txt"
+
+display_menu() {
+  echo "1. Create Student Record"
+  echo "2. View All Student Records"
+  echo "3. Delete Student Record"
+  echo "4. Update Student Record"
+  echo "5. Exit"
+  read -p "Choose an option: " option
+}
