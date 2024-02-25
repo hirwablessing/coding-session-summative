7a4ef354c5ac9f999938255a23d82171674f4c5b
Patrick Niyogitare <patrickniyogitare28@gmail.com>
2024-02-25 21:11:41 +0200

    Added bash command

--- move-to-directory.sh
+++ move-to-directory.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Move the files to the specified directory
 # Assuming the files are in the current directory and named file1, file2, file3, file4
 # Update the file names according to the files you need to move
