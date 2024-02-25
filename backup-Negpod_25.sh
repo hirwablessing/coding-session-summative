91a6b74d225f00329c7575f5746363a8a9ab15c5
Ange Nicole Mukundwa <55019052+AngeNicole@users.noreply.github.com>
2024-02-25 18:47:12 +0200

    add a security note

--- backup-Negpod_25.sh
+++ backup-Negpod_25.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# NOTE: Avoid storing plain-text passwords in scripts to prevent exposure and unauthorized access. 
+# Opt for secure alternatives like SSH keys for automation.
+
 # Configuration variables
 remote_host="64293e56bc62.3a2627c1.alu-cod.online"
 remote_username="64293e56bc62"
