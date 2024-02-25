20e639448e1030013a357e83a8e8d92b47a49b0a
Ange Nicole Mukundwa <55019052+AngeNicole@users.noreply.github.com>
2024-02-25 18:38:18 +0200

    Update backup-Negpod_25.sh

--- backup-Negpod_25.sh
+++ backup-Negpod_25.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# Define variables for the operation
-directory_name="negpod_25-q1"  # The directory to backup
-remote_host="64293e56bc62.3a2627c1.alu-cod.online" # Remote server host
+# Configuration variables
+remote_host="64293e56bc62.3a2627c1.alu-cod.online"
+remote_username="64293e56bc62"
