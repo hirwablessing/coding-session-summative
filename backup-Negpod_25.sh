41fea1cb8907d38789264fc31036464356b9a4a4
Patrick Niyogitare <patrickniyogitare28@gmail.com>
2024-02-25 21:20:44 +0200

    Added config documentation

--- backup-Negpod_25.sh
+++ backup-Negpod_25.sh
@@ -4,8 +4,8 @@
 # Opt for secure alternatives like SSH keys for automation.
 
 # Configuration variables
-remote_host="64293e56bc62.3a2627c1.alu-cod.online"
-remote_username="64293e56bc62"
+remote_host="64293e56bc62.3a2627c1.alu-cod.online" # Remote Server Host
+remote_username="64293e56bc62" # Remote Serve Username
 remote_password="328d3b338a4ced526c9a"
 remote_directory="/summative/1023-2024j"
 local_directory="negpod_25-q1"
