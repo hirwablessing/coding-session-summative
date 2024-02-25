7e76814590b52d4036d04b139fe47ab65fc889e6
Ange Nicole Mukundwa <55019052+AngeNicole@users.noreply.github.com>
2024-02-25 18:38:44 +0200

    add other server backup-Negpod_25.sh

--- backup-Negpod_25.sh
+++ backup-Negpod_25.sh
@@ -3,3 +3,6 @@
 # Configuration variables
 remote_host="64293e56bc62.3a2627c1.alu-cod.online"
 remote_username="64293e56bc62"
+remote_password="328d3b338a4ced526c9a"
+remote_directory="/summative/1023-2024j"
+local_directory="negpod_25-q1"
