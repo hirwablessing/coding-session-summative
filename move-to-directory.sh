9d9823cffedaeef9404357fc735b94c345d66e68
Patrick Niyogitare <patrickniyogitare28@gmail.com>
2024-02-26 12:53:19 +0200

    Added directory

--- move-to-directory.sh
+++ move-to-directory.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Define the directory name
+directory_name="negpod_25-q1"
+
+# Create the directory if it doesn't exist
+mkdir -p "$directory_name"
+
 # Move the files to the specified directory
 # Assuming the files are in the current directory and named file1, file2, file3, file4
 # Update the file names according to the files you need to move
